9a973089b4339b0d9ca95cc4cc08e7ea433a2b4f
Sergio <sergiosilva@outlook.com>
2022-03-02 02:06:09 -0300

    Finished Project!

--- js/index.js
+++ js/index.js
@@ -1,88 +1,82 @@
-// Player put rock, paper or scissor
-// Computer tankes randonly rock, paper or scissor
-// if player put rock and computer also put rock its a stealmate
-// if player put rock and computer put paper the computer wins
-// if player put rock and computer put scissor the player wins
-// if player put paper and computer put rock the player wins
-// if player put paper and computer put paper its a stealmate
-// if player put paper and computer put scissor the computer wins
-// if player put scissor and computer put paper the player wins
-// if player put scissor and computer put rock the computer wins
-// if player put scissor and computer put scissor its a stealmate
-// The game is defined after 5 rounds, wins who get more points
 
-
-let playerSelection = ""
-let computerSelection = ""
-let scorePlayer = ""
-let scorePc = "" 
-let whoWin = ""
-
-const btn = document.querySelectorAll(".btn")
 const result = document.querySelector(".result")
-console.log(result)
+const resultText = document.querySelector(".result-text")
+const playerPoints = document.querySelector(".player-result")
+const computerPoints = document.querySelector(".computer-result")
+const choice = document.querySelectorAll(".choice")
+const btnRst = document.getElementById("btn-reset")
+let playerChoice = ""
+let computerChoice = ""
+let playerScore = ""
+let computerScore = ""
+let isAlive = true;
 
-btn.forEach(btn => btn.addEventListener("click", function(e){
-    if(btn.textContent === "Rock"){
-        playerSelection = "rock"
-        game()        
-    } else if (btn.textContent === "Paper"){
-        playerSelection = "paper"
-        game()
-    } else {
-        playerSelection = "scissor"
-        game()
-    }
-}))
-
-
-
-
-function game(){      
-    
-         computerSelection = computerPlay()             
-        
-        function playRound(){
-            if (playerSelection === computerSelection){
-                whoWin = `Its a tie `
-            } else if ( playerSelection === "rock" && computerSelection === "scissor"){
-                whoWin = "You win! rock beats scissor"
-            } else if (playerSelection === "rock" && computerSelection === "paper") {
-                whoWin = "You lose! Paper beats Rock"
-            } else if (playerSelection === "paper" && computerSelection === "rock"){
-                whoWin = "You win! paper beats rock"
-            } else if (playerSelection === "paper" && computerSelection === "scissor"){
-                whoWin = "You lose! Scissor beats paper"
-            } else if (playerSelection === "scissor" && computerSelection === "paper"){
-                whoWin = "You win! Scissor beats paper"
-            } else if (playerSelection === "scissor" && computerSelection === "rock"){
-                whoWin = "You lose! Rock beats scissor!"
-            } else {
-                whoWin = "Put a valid option!"
-            }            
+choice.forEach(function(e){
+    e.addEventListener("click", function(){
+        if (e.textContent === "✋"){
+            playerChoice = "✋"
+        } else if (e.textContent === "✊"){
+            playerChoice = "✊"
+        } else {
+            playerChoice = "✌️"
         }
-        playRound()     
-        result.textContent = whoWin      
-    }      
-
+        game()        
+    })
+})
 
 function computerPlay(){
     let randomChoice = Math.floor(Math.random() *3 )   
     if (randomChoice === 0){
-        return "rock"
+        computerChoice =  "✋"
     } else if (randomChoice === 2){
-        return "paper"
+        computerChoice =  "✊"
     } else {
-        return "scissor"
+        computerChoice =  "✌️"
     }
 }
-  
 
+function game(){
+    computerPlay()    
+    if(isAlive){
+        if ( playerChoice === computerChoice){
+            resultText.textContent = "It's a tie!"
+        } else if ((playerChoice === "✊" && computerChoice === "✌️") || 
+                (playerChoice === "✋" && computerChoice === "✊") || 
+                (playerChoice === "✌️" && computerChoice === "✋")){             
+                resultText.textContent = `Player Wins! ${playerChoice} beats ${computerChoice}`;
+                result.textContent = `${playerChoice}` 
+                playerScore++
+                playerPoints.textContent = `${playerScore}`                     
+        } else {
+            resultText.textContent = `Computer Wins! ${computerChoice} beats ${playerChoice}`;
+            result.textContent = `${computerChoice}`
+            computerScore++
+            computerPoints.textContent = `${computerScore}`
+        }
+        gameIsOver()       
+    }    
 
+}  
+             
+function gameIsOver(){
+    if( playerScore === 5 ){
+        resultText.textContent = `Player Wins!`
+        isAlive = false
+    } else if (computerScore === 5) {
+        resultText.textContent = `Computer Wins!`
+        isAlive = false
+    }    
+}
 
-
-
-
-
-
-
+btnRst.addEventListener("click", function(){    
+        playerChoice = ""
+        computerChoice = ""
+        playerScore = ""
+        computerScore = ""
+        result.textContent = "✊"
+        resultText.textContent = "Let's Play!"
+        computerPoints.textContent = "0"
+        playerPoints.textContent = "0"
+        isAlive = true        
+    }    
+)
